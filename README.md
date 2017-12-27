d1ae629a2fdb9a7e053639da3dc6fdfd3ed69eac
Yujing Wu <yujingwu@gmail.com>
2017-12-26 17:39:11 -0800

    DX-1360 first drop of sort-of functional code

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
-# tutorial-java-WebSocketEventFeed
-WebSocket Event Feed: sample app monitor events sent to ARTIK cloud services that pertain to the specified source devices or user
+# Tutorial Java Event Feed WebSocket
+
+This is a starter code to connect to ARTIK Cloud [Event feed WebSocket (/live)](https://developer.artik.cloud/documentation/data-management/rest-and-websockets.html#event-feed-websocket) endpoint using ARTIK Cloud Java SDK.  By running this sample you will learn to:
+
+- Connect to the ARTIK Cloud Event feed WebSocket url.
+- Monitor realtime messages sent to ARTIK Cloud by the specified source devices.
+
+## Requirements
+
+- Java 7 or higher 
+- [Apache Maven](https://maven.apache.org/download.cgi)
+
+## Setup
+
+### Setup at ARTIK Cloud
+
+1. At My ARTIK Cloud, [connect a device](https://my.artik.cloud/new_device/cloud.artik.example.simple_smartlight) of the type "Example Simple Smart Light" (unique type name `cloud.artik.example.simple_smartlight`). You can use the one that you already own.
+
+2. Get the device ID for your newly created device in the [Device Info](https://developer.artik.cloud/documentation/tools/web-tools.html#managing-a-device-token) screen.
+3. Retrieve a [user token](https://developer.artik.cloud/documentation/tools/api-console.html#find-your-user-token) using our api-console, or via oauth2 authentication with your own application. 
+
+### Setup Java Project
+
+1. Clone this repository if you haven't already done so.
+
+2. At the root directory and run the command:
+
+   ```
+   mvn clean package
+   ```
+
+   The executable `websocket-monitor-x.x.jar` is generated under the target directory.
+
+3. Run the command at the target directory to learn the usage:
+
+   ```
+   java -jar websocket-monitor-x.x.jar
+   ```
+
+## Demo:
+
+1. Start and run the following command in the target directory:
+
+```
+java -jar websocket-monitor-x.x.jar -device YOUR_DEVICE_ID -token YOUR_USER_TOKEN
+```
+
+2. Send messages to your Example Simple Smart Light using the [Online Device Simulator](https://developer.artik.cloud/documentation/tools/web-tools.html#using-the-online-device-simulator).   Simulate the device with following settings:
+
+```
+* Simulate data on the boolean "state" field with default 5000 ms (5 secs).   
+* Alternative between true/false value by setting the data pattern to "alternating boolean".
+```
+
+2. In your running sample application you will see the received messages.    Here is the example output:
+
+```bash
+Connecting to: wss://api.artik.cloud/v1.1/live?authorization=bearer+aa176...&device=bb101...
+Status: CONNECTING ...
+Connection successful with code:[101]
+
+Received ping with ts:[1507328677960]
+
+Received message:[class MessageOut {
+    data: {state=true}
+    cid: null
+    ddid: null
+    sdid: bb101...
+    ts: 1507328710000
+    type: message
+    mid: ba31d3eb38e342a49226828ff1dac58d
+    uid: ff123...
+    sdtid: dtd1d3e0934d9348b783166938c0380128
+    cts: 1507328699982
+    mv: 1
+}]
+
+Received message:[class MessageOut {
+    data: {state=false}
+    cid: null
+    ddid: null
+    sdid: bb101...
+    ts: 1507328715000
+    type: message
+    mid: 8070d27294f04620a82b389c9e768aab
+    uid: ff123...
+    sdtid: dtd1d3e0934d9348b783166938c0380128
+    cts: 1507328704710
+    mv: 1
+}]
+Received ping with ts:[1507328707969]
+```
+
+3. Stop the simulation so it does not accrue any more data usage.
+
+## More about ARTIK Cloud
+
+If you are not familiar with ARTIK Cloud, we have extensive documentation at <https://developer.artik.cloud/documentation>
+
+The full ARTIK Cloud API specification can be found at <https://developer.artik.cloud/documentation/api-reference/>
+
+Peek into advanced sample applications at [https://developer.artik.cloud/documentation/tutorials/code-samples](https://developer.artik.cloud/documentation/tutorials/code-samples/)
+
+To create and manage your services and devices on ARTIK Cloud, visit the Developer Dashboard at [https://developer.artik.cloud](https://developer.artik.cloud/)
+
+## License and Copyright
+
+Licensed under the Apache License. See [LICENSE](./LICENSE).
+
+Copyright (c) 2017 Samsung Electronics Co., Ltd.
---
+++ src/main/java/cloud/artik/example/WebSocketEventMonitor.java
@@ -0,0 +1,191 @@
+package cloud.artik.example;
+
+import cloud.artik.model.Acknowledgement;
+import cloud.artik.model.ActionOut;
+import cloud.artik.model.EventFeedData;
+import cloud.artik.model.MessageOut;
+import cloud.artik.model.WebSocketError;
+import cloud.artik.websocket.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+
+/**
+* WebSocket starter code to connect to ARTIK Cloud Event(/events) endpoint.
+* 
+* ARTIK Cloud Java SDK:
+* https://github.com/artikcloud/artikcloud-java
+* 
+* Additional information available in our documentation:
+* https://developer.artik.cloud/documentation/api-reference/websockets-api.html
+* 
+*/
+
+
+public class WebSocketEventMonitor {
+	
+	private static final int MIN_EXPECTED_ARGUMENT_NUMBER = 3;
+	
+	static ArrayList<String> queryParams = new ArrayList<String>();
+
+	// comma delimited string of `device ID` of interest
+	static String sdids = null;     
+		
+	// here we are using the `user token` with sdids.  Or use `device token` associated device ID.
+	static String accessToken = null;
+		
+	// user ID associated with the access token.
+	static String uid = null;	
+	
+	// single `device ID` of interest wen
+	static String deviceId =  null;
+	
+	// parameters below not used in this sample and set to null
+	// for use when monitoring by device type
+	static String sdtids = null;    //comma delimited string of `device type ID` of interest
+	
+	
+	public static void main(String args[]) throws URISyntaxException, IOException {
+		
+		if (!succeedParseCommand(args)) {
+            printUsage();
+             return;
+        }
+		
+		// Use wild card to receive any events described in
+		// https://developer.artik.cloud/documentation/data-management/rest-and-websockets.html#event-feed-websocket
+		String events = "*";
+		
+		EventFeedWebSocketCallback eventCallback = new EventFeedWebSocketCallback() {
+			@Override
+			public void onEvent(EventFeedData event) {
+				System.out.println(String.format("Received event:[%s]", event));
+				
+			}
+		};
+		
+		ArtikCloudWebSocketCallback generalCallback = new ArtikCloudWebSocketCallback() {
+			@Override
+			public void onOpen(int code, String message) {
+				System.out.println(String.format("Connection successful with code:[%d]", code));
+			}
+			
+			@Override
+			public void onClose(int arg0, String arg1, boolean arg2) {
+				System.out.println(String.format("Connction closed with [%d][%s][%b]", arg0, arg1, arg2));
+			}
+		
+			@Override
+			public void onMessage(MessageOut message) {
+				System.out.println(String.format("Received message:[%s]", message));
+			}
+			
+			@Override
+			public void onAction(ActionOut actionOut) {
+				System.out.println(String.format("Received action:[%s]", actionOut));
+			}
+			
+			@Override
+			public void onPing(long ts) {
+				System.out.println(String.format("Received ping with ts:[%d]", ts));
+			}
+			
+			@Override
+			public void onError(WebSocketError error) {
+				System.out.println(String.format("Received error:[%s]", error));
+			}
+			
+			@Override
+			public void onAck(Acknowledgement acknowledgement) {
+				System.out.println(String.format("Received Ack [%s]", acknowledgement));
+			}
+		};
+		
+		// sets the parameters and callback function for the WebSocket connection
+		// must pass one of "one of deviceId, sdids, or uid" with user token"
+		EventFeedWebSocket ws = 
+				new EventFeedWebSocket(accessToken, deviceId, sdids, uid, events, generalCallback, eventCallback);
+		
+		
+		System.out.println(String.format("Connecting to: wss://api.artik.cloud/v1.1/live?authorization=bearer+%s", accessToken));
+		
+		System.out.println("With query parameters:");
+		
+		for(String params: queryParams ) {
+			 System.out.println("  "  + params);
+		}
+		
+		// fires the request to make WebSocket connection
+		ws.connect();
+				
+		System.out.println("Status: " + ws.getConnectionStatus());
+		
+		
+	}
+	
+	// Usage: event-monitor -token xxx <== token can be user token
+	private static boolean succeedParseCommand(String args[]) {
+	       if (args.length < MIN_EXPECTED_ARGUMENT_NUMBER) {
+	           return false; 
+	       }
+	       
+	       int index = 0;
+	       while (index < args.length) {
+	           String arg = args[index];
+	           
+	           if ("-sdids".equals(arg)) {
+	               ++index; // Move to the next argument, value for parameter
+	               sdids = args[index];
+	           } else if ("-token".equals(arg)) {
+	               ++index; // Move to the next argument, value for parameter
+	               accessToken = args[index];
+	           } else if ("-device".equals(arg)) {
+	        	   ++index; // Move to the next argument, value for parameter 
+	        	   deviceId = args[index];
+	           } else if ("-uid".equals(arg)) {
+	        	   ++index; // Move to the next argument, value for parameter 
+	        	   uid = args[index];
+	           } else {
+	        	   	        	   
+	        	   return false;
+	           }
+	           
+	           //store as query param format for printing without the '-' flag prefix
+	           queryParams.add(arg.substring(1) + "=" + args[index]);
+	           
+	           ++index;
+	       }
+	       
+	       // must provide a token
+	       if (accessToken == null ) {
+	    	   System.out.println("access token null");
+	           return false;
+	       }
+	       
+	       // one of device (device id), sdids (list of device ids), uid (user id) must be set
+	       if (deviceId == null && sdids == null && uid == null) {
+	    	   
+	    	   System.out.println("device id or sdids null");
+	    	   return false;
+	       }
+	       
+	       return true;
+	   }
+	
+	private static void printUsage() {
+	       System.out.println("Usage: java -jar websocket-monitor-x.x.jar" + " -sdids YOUR_DEVICE_ID -token YOUR_USER_TOKEN");
+	      
+	       System.out.println("One of following flag:");
+	       System.out.println("       -device to filter by single DEVICE_ID");
+	       System.out.println("       -sdids  to filter by multiple DEVICE_IDs, comma delimited (ie:  id1, id2, id3)");
+	       System.out.println("       -uid    to filter all devices of USER_ID");
+	       
+	       System.out.println("With token");
+	       System.out.println("       -token USER_TOKEN (required), allows also DEVICE_TOKEN when used with -device flag");
+	       
+	}
+
+}
+
